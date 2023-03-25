02847dcb65cc019d62e31c0832cb5ad5d76b8330
bassaoud <mohamed.bassaoud@yahoo.fr>
2023-03-25 17:34:28 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'HRMangement',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
